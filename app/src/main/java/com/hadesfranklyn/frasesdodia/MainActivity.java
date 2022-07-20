627fb670c47e4fa43bfd648552cc4f96e4b2c4c5
Franklyn R <franklyn1998@live.com>
2022-07-20 11:57:59 -0300

    Projeto finalizado

--- app/src/main/java/com/hadesfranklyn/frasesdodia/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/frasesdodia/MainActivity.java
@@ -1,8 +1,12 @@
 package com.hadesfranklyn.frasesdodia;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gerarNovaFrase(View view) {
+
+        String[] frases = {
+          "Que o dia comece bem e termine ainda melhor.",
+          "Pra hoje: sorrisos bobos, uma mente tranquila e um coração cheio de paz.",
+          "Às vezes as coisas demoram, mas acontecem. O importante é saber esperar e não perder a fé!",
+          "Imagine uma nova história para a sua vida e acredite nela.",
+          "Nem todos os dias são bons, mas há algo bom em cada dia."
+        };
+
+        TextView texto = findViewById(R.id.textResultado);
+        int numeroAleatorio0a5 = new Random().nextInt(5);
+        texto.setText(frases[numeroAleatorio0a5]);
+
     }
 }
